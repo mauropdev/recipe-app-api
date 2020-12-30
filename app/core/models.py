f2135f54de0fe6095e15e7ab890f4bc1d2172672
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2020-12-30 17:43:12 -0500

    Add support for creating superusers

--- app/core/models.py
+++ app/core/models.py
@@ -15,13 +15,22 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self,  email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
