c11e5f18faf96583800f7561c57dbb93d2823a03
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2020-12-31 10:05:14 -0500

    Modify Django admin to support changing user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,7 +17,7 @@ class AdminSiteTests(TestCase):
             password='password123',
             name='Test user full name'
         )
-    
+
     def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
@@ -25,3 +25,11 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args[self.user.id])
+        # /admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
