38ce8c5cf98a9d1bb64f735666ab6582f0f72ed7
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2020-12-31 12:14:59 -0500

    Fix for testinng password too short

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -35,7 +35,11 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating an user that already exists fails"""
-        payload = {'email': 'test@example.com', 'password': 'testpass'}
+        payload = {
+            'email': 'test@example.com', 
+            'password': 'testpass'
+            'name': 'test'
+        }
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -44,7 +48,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password the must be more than 5 characters"""
-        payload = {'email': 'test@example.com', 'password': 'pw'}
+        payload = {
+            'email': 'test@example.com', 
+            'password': 'pw',
+            'name': 'test'
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
